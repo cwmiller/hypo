3637c2ffbe700447a3b457c36145ae20d4f4f15c
Chase Miller <cmiller@tls.net>
2013-08-02 16:32:28 -0400

    Added configuration for travis-ci

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,10 @@
 <?php
-$autoloader = require __DIR__.'/../vendor/autoload.php';
+$autoloader = require(__DIR__.'/../vendor/autoload.php');
+
+if (!$autoloader) {
+	die('You must set up the project dependencies, run the following commands:
+	        wget http://getcomposer.org/composer.phar
+	        php composer.phar install');
+}
+
 $autoloader->add('CWM\Hypo\Tests', __DIR__);
